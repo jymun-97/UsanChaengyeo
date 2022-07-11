2cb9b066347988b67219598f2b892f141552058b
jymun-97 <mjy0750@gmail.com>
2022-07-12 00:11:29 +0900

    feat: clean architecture build-up

--- app/src/main/java/com/usanchaengyeo/usanchaengyeo/ui/MainActivity.kt
+++ app/src/main/java/com/usanchaengyeo/usanchaengyeo/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.usanchaengyeo.usanchaengyeo
+package com.usanchaengyeo.usanchaengyeo.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.usanchaengyeo.usanchaengyeo.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
