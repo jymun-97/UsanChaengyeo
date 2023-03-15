d7b4fe8ad44857680dec13c6856483656c0dde2e
jymun-97 <mjy0750@gmail.com>
2023-03-15 15:00:43 +0900

    feat: WeatherView 동작 설정(비 내리는 애니메이션)

--- app/src/main/java/com/jymun/usanchaengyeo/ui/base/BaseActivity.kt
+++ app/src/main/java/com/jymun/usanchaengyeo/ui/base/BaseActivity.kt
@@ -3,6 +3,8 @@ package com.jymun.usanchaengyeo.ui.base
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.ViewDataBinding
+import com.github.matteobattilana.weather.PrecipType
+import com.github.matteobattilana.weather.WeatherView
 import com.jymun.harusekki.ui.base.BaseViewModel
 
 abstract class BaseActivity<VM : BaseViewModel, B : ViewDataBinding> : AppCompatActivity() {
@@ -10,13 +12,28 @@ abstract class BaseActivity<VM : BaseViewModel, B : ViewDataBinding> : AppCompat
     abstract val viewModel: VM
 
     protected lateinit var binding: B
+    protected lateinit var weatherView: WeatherView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = getViewDataBinding()
         setContentView(binding.root)
+
+        weatherView = getWeatherViewInstance()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        weatherView.setWeatherData(PrecipType.RAIN)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        weatherView.resetWeather()
     }
 
     abstract fun getViewDataBinding(): B
+
+    abstract fun getWeatherViewInstance(): WeatherView
 }
