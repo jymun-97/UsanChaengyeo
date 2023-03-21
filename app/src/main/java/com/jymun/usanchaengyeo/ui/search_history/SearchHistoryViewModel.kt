d3732764068fe1a2dd9b72f494ff9403c29eaf2b
jymun-97 <mjy0750@gmail.com>
2023-03-21 22:24:14 +0900

    add: SearchHistoryFragment empty Layout & ViewModel

---
+++ app/src/main/java/com/jymun/usanchaengyeo/ui/search_history/SearchHistoryViewModel.kt
@@ -0,0 +1,14 @@
+package com.jymun.usanchaengyeo.ui.search_history
+
+import com.jymun.harusekki.ui.base.BaseViewModel
+import com.jymun.usanchaengyeo.util.dispatcher.DispatcherProvider
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class SearchHistoryViewModel @Inject constructor(
+    dispatcherProvider: DispatcherProvider
+) : BaseViewModel(dispatcherProvider) {
+
+
+}
