18b34f49312c4cdf3bf1f169fee34f48cd939adb
jymun-97 <mjy0750@gmail.com>
2023-03-21 17:00:25 +0900

    feat: 키워드로 주소 검색 서비스 추가

--- app/src/main/java/com/jymun/usanchaengyeo/data/service/AddressService.kt
+++ app/src/main/java/com/jymun/usanchaengyeo/data/service/AddressService.kt
@@ -1,6 +1,7 @@
 package com.jymun.usanchaengyeo.data.service
 
 import com.jymun.usanchaengyeo.data.entity.address.AddressName
+import com.jymun.usanchaengyeo.data.entity.address.AddressResponse
 import com.jymun.usanchaengyeo.data.service.NetworkConstant.ADDRESS_SERVICE_KEY
 import retrofit2.Response
 import retrofit2.http.GET
@@ -15,4 +16,10 @@ interface AddressService {
         @Query("x") longitude: Double,
         @Query("y") latitude: Double
     ): Response<AddressName>
+
+    @Headers("Authorization: KakaoAK $ADDRESS_SERVICE_KEY")
+    @GET("/v2/local/search/keyword")
+    suspend fun searchAddressByKeyword(
+        @Query("query") keyword: String
+    ): Response<AddressResponse>
 }
