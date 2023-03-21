f9f16a130c7a76ee5557a6f47095c3644ba9f349
jymun-97 <mjy0750@gmail.com>
2023-03-21 17:02:09 +0900

    add: Address Model (뷰에서 표기할 주소 모델)

---
+++ app/src/main/java/com/jymun/usanchaengyeo/data/model/address/Address.kt
@@ -0,0 +1,33 @@
+package com.jymun.usanchaengyeo.data.model.address
+
+import com.jymun.usanchaengyeo.data.entity.address.AddressEntity
+import com.jymun.usanchaengyeo.data.model.Model
+import com.jymun.usanchaengyeo.data.model.ModelType
+
+data class Address(
+    override val id: Long,
+    override val type: ModelType = ModelType.ADDRESS,
+    private val placeName: String,
+    private val addressName: String,
+    private val roadAddressName: String,
+    private val x: String,
+    private val y: String
+) : Model(id, type) {
+
+    val addressText = placeName.ifEmpty { roadAddressName.ifEmpty { addressName } }
+    val subAddressText = when (addressText) {
+        placeName -> roadAddressName.ifEmpty { addressText }
+
+        roadAddressName -> addressText
+
+        else -> ""
+    }
+
+    fun toEntity() = AddressEntity(
+        addressName = addressName,
+        placeName = placeName,
+        roadAddressName = roadAddressName,
+        x = x,
+        y = y
+    )
+}
