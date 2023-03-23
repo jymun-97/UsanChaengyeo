d4b9b70a3e961f8f1ae6469c7b9b2a4e51259249
jymun-97 <mjy0750@gmail.com>
2023-03-23 19:59:18 +0900

    feat: 사용자가 입력한 검색 키워드에 따라 검색 결과(List<Address>) 표기

--- app/src/main/java/com/jymun/usanchaengyeo/ui/MainViewModel.kt
+++ app/src/main/java/com/jymun/usanchaengyeo/ui/MainViewModel.kt
@@ -2,9 +2,11 @@ package com.jymun.usanchaengyeo.ui
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.switchMap
 import com.jymun.harusekki.ui.base.BaseViewModel
 import com.jymun.usanchaengyeo.data.model.address.Address
 import com.jymun.usanchaengyeo.domain.address.CoordinateToAddressUseCase
+import com.jymun.usanchaengyeo.domain.address.SearchAddressUseCase
 import com.jymun.usanchaengyeo.util.dispatcher.DispatcherProvider
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -13,6 +15,7 @@ import javax.inject.Inject
 class MainViewModel @Inject constructor(
     dispatcherProvider: DispatcherProvider,
     private val coordinateToAddressUseCase: CoordinateToAddressUseCase,
+    private val searchAddressUseCase: SearchAddressUseCase
 ) : BaseViewModel(dispatcherProvider) {
 
     private val _selectedAddress = MutableLiveData<Address?>(null)
@@ -20,6 +23,17 @@ class MainViewModel @Inject constructor(
         get() = _selectedAddress
 
     val searchKeyword = MutableLiveData<String>()
+
+    val searchResult = searchKeyword.switchMap { keyword ->
+        val result = MutableLiveData<List<Address>?>()
+        onMainDispatcher {
+            result.postValue(
+                if (keyword.isEmpty()) emptyList()
+                else searchAddressUseCase(keyword)
+            )
+        }
+        result
+    }
 
     fun coordinateToAddress(
         longitude: Double,
--- app/src/main/java/com/jymun/usanchaengyeo/ui/search_address/SearchAddressFragment.kt
+++ app/src/main/java/com/jymun/usanchaengyeo/ui/search_address/SearchAddressFragment.kt
@@ -1,19 +1,43 @@
 package com.jymun.usanchaengyeo.ui.search_address
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.jymun.usanchaengyeo.data.model.address.Address
 import com.jymun.usanchaengyeo.databinding.FragmentSearchAddressBinding
 import com.jymun.usanchaengyeo.ui.MainViewModel
 import com.jymun.usanchaengyeo.ui.base.BaseFragment
+import com.jymun.usanchaengyeo.ui.base.adapter.ModelRecyclerAdapter
+import com.jymun.usanchaengyeo.util.resources.ResourcesProvider
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class SearchAddressFragment : BaseFragment<MainViewModel, FragmentSearchAddressBinding>() {
+
+    @Inject
+    lateinit var resourcesProvider: ResourcesProvider
 
     override val viewModel: MainViewModel by activityViewModels()
 
     override fun getViewDataBinding() = FragmentSearchAddressBinding.inflate(layoutInflater)
 
-    override fun setUpBinding() = binding.apply { }
+    override fun setUpBinding() = binding.apply {
+        viewModel = this@SearchAddressFragment.viewModel
+        lifecycleOwner = viewLifecycleOwner
+    }
 
     override fun observeState() {}
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initAddressRecyclerView()
+    }
+
+    private fun initAddressRecyclerView() = binding.addressRecyclerView.apply {
+        layoutManager = LinearLayoutManager(requireActivity())
+        adapter = ModelRecyclerAdapter<Address>(resourcesProvider)
+    }
 }
