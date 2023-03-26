33cf83d96da4862f88a0281687f794a918d08355
jymun-97 <mjy0750@gmail.com>
2023-03-26 23:54:51 +0900

    chore: register forecast api key

--- app/src/main/java/com/jymun/usanchaengyeo/data/service/NetworkConstant.kt
+++ app/src/main/java/com/jymun/usanchaengyeo/data/service/NetworkConstant.kt
@@ -6,4 +6,7 @@ object NetworkConstant {
 
     const val ADDRESS_BASE_URL = "https://dapi.kakao.com"
     const val ADDRESS_SERVICE_KEY = BuildConfig.address_service_key
+
+    const val FORECAST_BASE_URL = "http://apis.data.go.kr"
+    const val FORECAST_API_KEY = BuildConfig.forecast_api_key
 }
