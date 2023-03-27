249f5df220936cd3c7ed2a4cc046f7fdec8a4249
jymun-97 <mjy0750@gmail.com>
2023-03-27 16:26:00 +0900

    add: 강수 범위에 따른 데이터를 나타낼 ForecastInfo Model

---
+++ app/src/main/java/com/jymun/usanchaengyeo/data/model/forecast/ForecastInfo.kt
@@ -0,0 +1,13 @@
+package com.jymun.usanchaengyeo.data.model.forecast
+
+import com.jymun.usanchaengyeo.data.model.Model
+import com.jymun.usanchaengyeo.data.model.ModelType
+
+data class ForecastInfo(
+    override val id: Long,
+    override val type: ModelType = ModelType.FORECAST_INFO,
+    val start: Int,
+    val end: Int,
+    val description: String,
+    val comment: String
+) : Model(id, type)
