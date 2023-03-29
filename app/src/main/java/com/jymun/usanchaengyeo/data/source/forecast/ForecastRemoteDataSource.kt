d07b091d84474e7f260a2b3a808679aabd6cd6b3
jymun-97 <mjy0750@gmail.com>
2023-03-29 19:52:48 +0900

    feat: ForecastRemoteDataSource에서 response가 성공적이지 않거나 body가 비어있으면 exception 발생

--- app/src/main/java/com/jymun/usanchaengyeo/data/source/forecast/ForecastRemoteDataSource.kt
+++ app/src/main/java/com/jymun/usanchaengyeo/data/source/forecast/ForecastRemoteDataSource.kt
@@ -4,6 +4,7 @@ import com.jymun.usanchaengyeo.data.entity.forecast.ForecastResponse
 import com.jymun.usanchaengyeo.data.service.ForecastService
 import com.jymun.usanchaengyeo.di.data.NetworkModule
 import com.jymun.usanchaengyeo.util.dispatcher.DispatcherProvider
+import com.jymun.usanchaengyeo.util.exception.CustomExceptions
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
@@ -26,6 +27,14 @@ class ForecastRemoteDataSource @Inject constructor(
             x = x,
             y = y
         )
+
+        if (!response.isSuccessful || response.body() == null) throw CustomExceptions.FailToConnectServerException
+        if (response.body()!!.resultCode != FORECAST_SUCCESS_CODE) throw CustomExceptions.FailToLoadCurrentLocationException
+
         return@withContext response.body()!!
+    }
+
+    companion object {
+        private const val FORECAST_SUCCESS_CODE = "00"
     }
 }
