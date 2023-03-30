f9d3a397d7f41de3e27ce924a8a21b5733e11f85
jymun-97 <mjy0750@gmail.com>
2023-03-31 00:49:27 +0900

    feat: 설정 화면의 스피너 혹은 슬라이더 이벤트 발생 시 설정 저장 및 로드 후 UI 갱신

--- app/src/main/java/com/jymun/usanchaengyeo/ui/setting/SettingActivity.kt
+++ app/src/main/java/com/jymun/usanchaengyeo/ui/setting/SettingActivity.kt
@@ -1,16 +1,18 @@
 package com.jymun.usanchaengyeo.ui.setting
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.AdapterView
+import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.viewModels
 import com.github.matteobattilana.weather.WeatherView
 import com.jymun.usanchaengyeo.R
+import com.jymun.usanchaengyeo.data.model.weather.WeatherData
 import com.jymun.usanchaengyeo.databinding.ActivitySettingBinding
 import com.jymun.usanchaengyeo.ui.base.BaseActivity
-import com.jymun.usanchaengyeo.ui.setting.weather.WeatherAdapter
-import com.jymun.usanchaengyeo.ui.setting.weather.WeatherData
+import com.jymun.usanchaengyeo.ui.base.LoadState
 import com.jymun.usanchaengyeo.util.resources.ResourcesProvider
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -21,15 +23,27 @@ class SettingActivity : BaseActivity<SettingViewModel, ActivitySettingBinding>()
     @Inject
     lateinit var resourcesProvider: ResourcesProvider
 
+    private var userIsInteracting = false
+    private lateinit var weatheradapter: WeatherAdapter
+
     override val viewModel: SettingViewModel by viewModels()
 
     override fun setUpBinding() = binding.apply {
-
+        viewModel = this@SettingActivity.viewModel
+        lifecycleOwner = this@SettingActivity
     }
 
     override fun getViewDataBinding() = ActivitySettingBinding.inflate(layoutInflater)
 
-    override fun observeState() {}
+    override fun observeState() = viewModel.loadState.observe(this) {
+        if (it is LoadState.Error) {
+            Toast.makeText(
+                this,
+                resourcesProvider.getString(it.exception.messageResId),
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
 
     override fun getWeatherViewInstance(): WeatherView = binding.weatherView
 
@@ -41,6 +55,14 @@ class SettingActivity : BaseActivity<SettingViewModel, ActivitySettingBinding>()
         initSizeSlider()
         initAmountSlider()
         initSetWeatherSpinner()
+
+        viewModel.loadWeatherData()
+        viewModel.weatherData.observe(this) {
+            val data = it ?: return@observe
+
+            Log.d("# SettingActivity", "$data")
+            bindViewsWithWeatherData(data)
+        }
     }
 
     private val backPressedCallback = object : OnBackPressedCallback(true) {
@@ -51,20 +73,20 @@ class SettingActivity : BaseActivity<SettingViewModel, ActivitySettingBinding>()
     }
 
     private fun initSpeedSlider() = binding.speedSlider.apply {
-        addOnChangeListener { _, value, _ ->
-            binding.weatherView.speed = value.toInt() * 20
+        addOnChangeListener { _, value, fromUser ->
+            if (fromUser) viewModel.updateSpeed(value)
         }
     }
 
     private fun initSizeSlider() = binding.sizeSlider.apply {
-        addOnChangeListener { _, value, _ ->
-            binding.weatherView.scaleFactor = 1 + value * 0.03f
+        addOnChangeListener { _, value, fromUser ->
+            if (fromUser) viewModel.updateSize(value)
         }
     }
 
     private fun initAmountSlider() = binding.amountSlider.apply {
-        addOnChangeListener { _, value, _ ->
-            binding.weatherView.emissionRate = value
+        addOnChangeListener { _, value, fromUser ->
+            if (fromUser) viewModel.updateAmount(value)
         }
     }
 
@@ -73,7 +95,7 @@ class SettingActivity : BaseActivity<SettingViewModel, ActivitySettingBinding>()
         adapter = WeatherAdapter(
             resourcesProvider,
             WeatherData.values
-        )
+        ).also { weatheradapter = it }
         onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onNothingSelected(parent: AdapterView<*>?) {}
 
@@ -83,15 +105,35 @@ class SettingActivity : BaseActivity<SettingViewModel, ActivitySettingBinding>()
                 position: Int,
                 id: Long
             ) {
-                bindWeatherData(adapter.getItem(position) as WeatherData)
+                if (userIsInteracting) viewModel.updateWeatherData(adapter.getItem(position) as WeatherData)
             }
         }
     }
 
-    private fun bindWeatherData(weatherData: WeatherData) = binding.apply {
-        weatherView.precipType = weatherData.weather
-        speedSlider.value = weatherData.defaultSpeed
-        sizeSlider.value = weatherData.defaultSize
-        amountSlider.value = weatherData.defaultAmount
+    private fun bindViewsWithWeatherData(weatherData: WeatherData) {
+        bindWeatherView(weatherData)
+        bindSliders(weatherData)
+        bindSpinner(weatherData)
+    }
+
+    private fun bindWeatherView(weatherData: WeatherData) = binding.weatherView.apply {
+        precipType = weatherData.weather
+        speed = weatherData.speed.toInt() * 20
+        scaleFactor = 1 + weatherData.size * 0.03f
+        emissionRate = weatherData.amount
+    }
+
+    private fun bindSliders(weatherData: WeatherData) = binding.apply {
+        speedSlider.value = weatherData.speed
+        sizeSlider.value = weatherData.size
+        amountSlider.value = weatherData.amount
+    }
+
+    private fun bindSpinner(weatherData: WeatherData) =
+        binding.setWeatherSpinner.setSelection(2 - weatherData.weather.ordinal)
+
+    override fun onUserInteraction() {
+        super.onUserInteraction()
+        userIsInteracting = true
     }
 }
